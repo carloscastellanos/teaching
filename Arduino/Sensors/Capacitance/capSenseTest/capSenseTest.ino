535479d56aba9d1ae4d729fdee8cf54830abba25
Carlos Castellanos <carloscastellanossf@gmail.com>
2020-08-08 01:47:11 -0400

    updated Capacitance

---
+++ Arduino/Sensors/Capacitance/capSenseTest/capSenseTest.ino
@@ -0,0 +1,18 @@
+#include <CapacitiveSensor.h>
+
+// 50k - 50M resistor between pins 2 & 3, pin 3 is sensor (receive) pin
+CapacitiveSensor sensor = CapacitiveSensor(2,3);
+
+void setup() {
+  // turn off autocalibrate on channel 1 - just as an example
+  sensor.set_CS_AutocaL_Millis(0xFFFFFFFF);
+  Serial.begin(9600);
+}
+
+void loop() {
+  long start = millis();                    // time performance - not necessary 
+  long total = sensor.capacitiveSensor(30); // read 30 bytes
+  
+  Serial.println(total);                    // print sensor output
+  delay(1);                                 // small delay to spare serial port from too much data
+}
