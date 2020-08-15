4c258feda0054d598367567fbe0d799d7a3fafd8
Carlos Castellanos <carloscastellanossf@gmail.com>
2020-08-15 15:19:21 -0400

    Update HC-SR04_test.ino

--- Arduino/Sensors/Ultrasonic/HC-SR04_test/HC-SR04_test.ino
+++ Arduino/Sensors/Ultrasonic/HC-SR04_test/HC-SR04_test.ino
@@ -35,7 +35,6 @@ void loop()
   // Read the signal from the sensor: a HIGH pulse whose
   // duration is the time (in microseconds) from the sending
   // of the ping to the reception of its echo off of an object.
-  pinMode(echoPin, INPUT);
   duration = pulseIn(echoPin, HIGH);
  
   // convert the time into a distance
