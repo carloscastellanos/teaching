2dd7a38534a9b57d4cd9a9e0c85e88f8b562704e
Carlos Castellanos <carloscastellanossf@gmail.com>
2025-04-06 23:14:27 -0400

    .

--- 480/Spring 2025/examples/locar/devorient/src/main.js
+++
@@ -1,4 +0,0 @@
-window.addEventListener("deviceorientationabsolute", e => {
-    document.getElementById("devorient").innerHTML = 
-        `Alpha: ${Math.floor(e.alpha)}<br />Beta: ${Math.floor(e.beta)}<br />Gamma: ${Math.floor(e.gamma)}`;
-});
